b69add5c6d4a7aaf84f39a88c376933add7c4f66
Siddhartha Hazra <siddhartha.it2019@nsec.ac.in>
2022-08-23 15:52:00 +0530

    added keydown

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 var drumButtonSize=document.querySelectorAll(".drum").length;
 
+function playsound(val){
+    switch (val) {
+        case "w":
+            var crash=new Audio('../sounds/crash.mp3');
+            crash.play();
+            break;
+            case "a":
+            var kickBass=new Audio('../sounds/kick-bass.mp3');
+            kickBass.play();
+            break;
+            case "s":
+                var snare=new Audio('../sounds/snare.mp3');
+                snare.play();
+                break;
+                case "d":
+                    var tom1=new Audio('../sounds/tom-1.mp3');
+                    tom1.play();
+                    break;
+                    case "j":
+                        var tom2=new Audio('../sounds/tom-2.mp3');
+                        tom2.play();
+                        break;
+        default:
+            break;
+    }
+}
+
+document.addEventListener("keydown",function(e){
+    playsound(e.key);
+});
+
 for(var i=0;i<drumButtonSize;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-        console.log(val);
-        switch (val) {
-            case "w":
-                var crash=new Audio('../sounds/crash.mp3');
-                crash.play();
-                break;
-                case "a":
-                var kickBass=new Audio('../sounds/kick-bass.mp3');
-                kickBass.play();
-                break;
-                case "s":
-                    var snare=new Audio('../sounds/snare.mp3');
-                    snare.play();
-                    break;
-                    case "d":
-                        var tom1=new Audio('../sounds/tom-1.mp3');
-                        tom1.play();
-                        break;
-                        case "j":
-                            var tom2=new Audio('../sounds/tom-2.mp3');
-                            tom2.play();
-                            break;
-            default:
-                break;
-        }
+        var val=this.innerHTML;
+        playsound(val);
     });
 }
