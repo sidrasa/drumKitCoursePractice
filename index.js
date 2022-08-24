e601260b6300ae14c8303f22e9446f19827bafec
Siddhartha Hazra <siddhartha.it2019@nsec.ac.in>
2022-08-24 13:53:56 +0530

    last 2 buttons

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ function playsound(val){
                         var tom2=new Audio('../sounds/tom-2.mp3');
                         tom2.play();
                         break;
+                        case "k":
+                            var tom3=new Audio('../sounds/tom-3.mp3');
+                            tom3.play();
+                            break;
+                    case "l":
+                        var tom4=new Audio('../sounds/tom-4.mp3');
+                        tom4.play();
+                        break; 
         default:
             break;
     }
